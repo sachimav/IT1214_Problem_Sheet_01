6420d6892647d6505bc86ce008155a733b97c55e
Sachintha Tissera <thiserasachintha@gmail.com>
2025-05-21 12:25:16 +0000

    fix bugs

--- 03.java
+++ 03.java
@@ -14,19 +14,19 @@ class  BankAccount{
     public int getaccountNumber(){
         return accountNumber;
     }
-    public int setaccountNumber(int accountNumber){
+    public void setaccountNumber(int accountNumber){
         this.accountNumber = accountNumber;
     }
     public String getaccountHolder(){
         return accountHolder;
     }
-    public String setaccountHolder(String accountHolder){
+    public void setaccountHolder(String accountHolder){
         this.accountHolder = accountHolder;
     }
     public double getbalance(){
         return balance;
     }
-    public double setbalance(int balance){
+    public void setbalance(int balance){
         this.balance = balance;
     }
 
@@ -37,13 +37,13 @@ class  BankAccount{
         balance -= amount;
     }
 }
-class Bank{
+class Bank {
 
     Scanner sc =new Scanner(System.in);
 
     static int[] accountNumberarry = new int[5];
     static String[] accountHolderarry = new String[5];
-    static int[] balancearry = new int[5];
+    static double[] balancearry = new double[5];
 
     int numbeofbankaccounts = 0;
     char choice;
@@ -52,7 +52,7 @@ class Bank{
         do{
             System.out.println("-----add new account------");
             System.out.println("enter account number: ");
-            accountHolderarry[numbeofbankaccounts] = sc.nextInt();
+            accountNumberarry[numbeofbankaccounts] = sc.nextInt();
 
             sc.nextLine();
             System.out.println("enter account holdaer name:");
@@ -63,21 +63,44 @@ class Bank{
 
             numbeofbankaccounts++;
 
-            System.out.println("do you want add new account y || n");
-            choice = sc.nextChar();
+            System.out.print("Do you want to add another account (y/n)? ");
+            sc.nextLine(); // <-- consume the leftover newline
+            String line = sc.nextLine();
+            if (line.isEmpty()) {
+                choice = 'n'; // default to 'n' if user enters nothing
+            } else {
+                choice = line.charAt(0);
+            }
 
         }
         while(choice == 'y');
     }
-    void Display(){
-        for(int i = 0 ; i<5 ; i++){
-            System.out.println("Accountnum\t holder\t balance");
-            System.out.println(accountNumberarry[i]+"\t "+accountHolderarry[i]+"\t"+balancearry[i]);
+    public void withdrawFromAccount() {
 
+        BankAccount bank = new BankAccount(01,"sst",30.2);
+
+        System.out.println("Enter account number 0-4");
+        int accNum = sc.nextInt();
+
+        System.out.println("Enter amount to withdraw: ");
+        double amounta = sc.nextDouble();
+        
+        bank.withdraw(balancearry[accNum]);
+
+
+
+    }
+    void Display(){
+         for (int i = 0; i < 5; i++) {
+            
+            BankAccount v = new BankAccount(accountNumberarry[i],accountHolderarry[i],balancearry[i]);
+            System.out.println(v.getaccountNumber() + "\t" + v.getaccountHolder() + "\t\t" + v.getbalance());
         }
     }
 
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
 
         Bank obj = new Bank();
 
@@ -87,8 +110,8 @@ class Bank{
             System.out.println("----welcome to Bank");
             System.out.println("1. Add new bank account");
             System.out.println("2.Withdraw");
-             System.out.println("4. Display Accounts");
-            System.out.println("3.exist");
+             System.out.println("3. Display Accounts");
+            System.out.println("4.exist");
 
             choose = sc.nextInt();
 
@@ -96,14 +119,17 @@ class Bank{
                 obj.addnewAccount();
             }
             else if(choose == 2){
-                obj.Withdraw();
+                obj.withdrawFromAccount();
             }
             else if(choose == 3){
                 obj.Display();
             }
+            else{
+                break;
+            }
 
         }
-        while(choose != 3 );
+        while(choose != 4 );
 
     }
 }
