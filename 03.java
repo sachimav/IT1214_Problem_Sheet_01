09d052b2ddcdbe34a8be3c744e90a5dd5cd00d66
Sachintha Tissera <thiserasachintha@gmail.com>
2025-05-30 21:49:46 +0530

    improve code

--- 03.java
+++ 03.java
@@ -38,98 +38,50 @@ class  BankAccount{
     }
 }
 class Bank {
+    private BankAccount[] accounts = new BankAccount[5];
+    private int count = 0;
 
-    Scanner sc =new Scanner(System.in);
-
-    static int[] accountNumberarry = new int[5];
-    static String[] accountHolderarry = new String[5];
-    static double[] balancearry = new double[5];
-
-    int numbeofbankaccounts = 0;
-    char choice;
-
-    void addnewAccount(){
-        do{
-            System.out.println("-----add new account------");
-            System.out.println("enter account number: ");
-            accountNumberarry[numbeofbankaccounts] = sc.nextInt();
-
-            sc.nextLine();
-            System.out.println("enter account holdaer name:");
-            accountHolderarry[numbeofbankaccounts]= sc.nextLine();
-
-            System.out.println("enter balance:");
-            balancearry[numbeofbankaccounts]= sc.nextInt();
-
-            numbeofbankaccounts++;
-
-            System.out.print("Do you want to add another account (y/n)? ");
-            sc.nextLine(); // <-- consume the leftover newline
-            String line = sc.nextLine();
-            if (line.isEmpty()) {
-                choice = 'n'; // default to 'n' if user enters nothing
-            } else {
-                choice = line.charAt(0);
-            }
-
-        }
-        while(choice == 'y');
-    }
-    public void withdrawFromAccount() {
-
-        BankAccount bank = new BankAccount(01,"sst",30.2);
-
-        System.out.println("Enter account number 0-4");
-        int accNum = sc.nextInt();
-
-        System.out.println("Enter amount to withdraw: ");
-        double amounta = sc.nextDouble();
-        
-        bank.withdraw(balancearry[accNum]);
-
-
-
-    }
-    void Display(){
-         for (int i = 0; i < 5; i++) {
-            
-            BankAccount v = new BankAccount(accountNumberarry[i],accountHolderarry[i],balancearry[i]);
-            System.out.println(v.getaccountNumber() + "\t" + v.getaccountHolder() + "\t\t" + v.getbalance());
+    public void addAccount(BankAccount account) {
+        if (count < 5) {
+            accounts[count] = account;
+            count++;
+        } else {
+            System.out.println("Bank is full. Cannot add more accounts.");
         }
     }
 
-    public static void main(String[] args){
-
-        Scanner sc = new Scanner(System.in);
-
-        Bank obj = new Bank();
-
-        int choose;
-
-        do{
-            System.out.println("----welcome to Bank");
-            System.out.println("1. Add new bank account");
-            System.out.println("2.Withdraw");
-             System.out.println("3. Display Accounts");
-            System.out.println("4.exist");
-
-            choose = sc.nextInt();
-
-            if(choose == 1){
-                obj.addnewAccount();
+    public void withdrawFromAccount(int accNum, double amount) {
+        for (int i = 0; i < count; i++) {
+            if (accounts[i].getaccountNumber() == accNum) {
+                try {
+                    accounts[i].withdraw(amount);
+                    System.out.println("Withdrawal successful for account " + accNum);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error for account " + accNum + ": " + e.getMessage());
+                }
+                return;
             }
-            else if(choose == 2){
-                obj.withdrawFromAccount();
-            }
-            else if(choose == 3){
-                obj.Display();
-            }
-            else{
-                break;
-            }
-
         }
-        while(choose != 4 );
+        System.out.println("Account " + accNum + " not found.");
+    }
 
+    public void displayAccounts() {
+        System.out.println("acconut\t holder\t balance");
+        for (int i = 0; i < count; i++) {
+            System.out.println(accounts[i].getaccountNumber() +"\t " + accounts[i].getaccountHolder() +"\t "+ accounts[i].getbalance());
+        }
+    }
+
+     public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        bank.addAccount(new BankAccount(1001, "Alice", 5000.0));
+        bank.addAccount(new BankAccount(1002, "Bob", 3000.0));
+
+
+        bank.withdrawFromAccount(1001, 6000.0);
+        bank.withdrawFromAccount(1002, 1000.0); 
+
+        bank.displayAccounts();
     }
 }
